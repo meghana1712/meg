5303fe04d05cf4a3a2164efb307bf122f76aa26c
meghana1712 <41179483+meghana1712@users.noreply.github.com>
2018-07-13 11:27:41 +0530

    Add files via upload

---
+++ j.java
@@ -0,0 +1,8 @@
+class add{
+        public static void main(String args[]){
+         int a=10;
+         int b=20;
+         int c=a+b;
+         System.out.println("the sum is"+c);
+       }
+       }
